95cdaa7c2d414132233620e3bfa3383f267a9c2a
YOUR NAME <YOUR@EMAIL.com>
2015-11-19 20:11:41 +0100

    second commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def about
+  		#@Felix = "My name is Felix"
+  		#render :contact
+  end
+
+  def contact
+  		#@Felix = "Felix The Awesome"
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+  	@posts = Post.all
+  end
+
+  def new 
+  	@post = Post.new
+  end 
+
+  def create 
+  	@post = Post.new(post_params)
+  	@post.save
+  	redirect_to post_path(@post)
+  end
+
+
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  private 
+
+  def post_params
+  	params.require(:post).permit(:title, :body)
+  end 
+
+end
