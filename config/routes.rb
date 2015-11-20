981e6ec35720d14ff16f3f5f7d7cde28de602cec
YOUR NAME <YOUR@EMAIL.com>
2015-11-20 17:00:08 +0100

    21st build

--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  
-  root 'posts#index' #home
+
+  root 'pages#about' #home
 
 
 
