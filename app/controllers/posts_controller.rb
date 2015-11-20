1407548f2ce22240ea3f7f8155b887b11efb8fe3
YOUR NAME <YOUR@EMAIL.com>
2015-11-20 23:19:21 +0100

    24th build

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy] #replaces @post = Post.find(params[:id])
+
   def index
   	@posts = Post.all.order('created_at DESC')
   end
@@ -15,30 +17,35 @@ class PostsController < ApplicationController
   end
 
   def edit 
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
   end  
 
   def update
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
     @post.update(post_params)
     flash[:notice] ='Post successfully updated!'
     redirect_to post_path(@post)
   end 
 
   def destroy 
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
     @post.destroy
     flash[:notice] ='Post deleted.'
     redirect_to posts_path
   end 
 
   def show
-  	@post = Post.find(params[:id])
+  	#@post = Post.find(params[:id])
   end
 
 
 
   private 
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 
   def post_params
   	params.require(:post).permit(:title, :body)
