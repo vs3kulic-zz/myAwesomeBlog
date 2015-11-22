17181302551c5a3b0f12fb56c6d493c8568e0f4f
YOUR NAME <YOUR@EMAIL.com>
2015-11-22 20:47:13 +0100

    20151122_2047

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.cache_classes = true
   config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
+  config.assets.digest = true
 
 end
