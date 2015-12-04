d5737e9e8af082a2f6f4f35e5b9dfcc8fbdf6416
vs3kulic <v.sekulic@gmx.at>
2015-12-04 18:16:49 +0100

    allow heroku auto assets precompilation

--- public/assets/application-70e2c8a1d4730db33a085ed769ef028410969b7ed363cd10acc681a9188228b8.js
+++ public/assets/application-70e2c8a1d4730db33a085ed769ef028410969b7ed363cd10acc681a9188228b8.js
@@ -12825,6 +12825,7 @@ $(function() {
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
+            
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
@@ -13833,6 +13834,10 @@ jQuery(document).ready(function($) {
             });
     }
 });
+(function() {
+
+
+}).call(this);
 (function() {
 
 
